be3f15d2cc877c5678de5987d3e0b77694ff35b1
h1s0s <hs94122@naver.com>
2023-02-09 11:23:28 +0900

    [한상선] 스프링 MVC 1편 - 스프링 MVC 기본기능 - HTTP 요청 메시지 - 단순 텍스트

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -2,15 +2,18 @@ package hello.springmvc.basic.request;
 
 import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Slf4j
@@ -102,5 +105,53 @@ public class RequestParamController {
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
 
         return helloData.toString();
+    }
+
+    /**
+     * HttpServletRequst에서 getInputStream()으로 읽어와서 문자열로 변환해서 읽을 수 있다.
+     */
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String string = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", string);
+
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * 매개변수에서 바로 inputStream 과 writer를 받을수도 있다.
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer writer) throws IOException {
+        String string = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", string);
+
+        writer.write("ok");
+    }
+
+    /**
+     * HttpEntity를 사용해서 더 편리하게 조회가 가능하다.
+     */
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity){
+
+        log.info("messageBody={}", httpEntity.getBody());
+
+        return new HttpEntity<>("ok");
+    }
+
+    /**
+     * @RequestBody라는 애노테이션을 이용해 더 간편하게 요청 메세지 바디를 받을수도 있다.
+     */
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String body){
+
+        log.info("messageBody={}", body);
+
+        return "ok";
     }
 }
