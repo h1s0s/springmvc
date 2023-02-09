7955097d2ab334f73d6b9b6da89072695e457a14
h1s0s <hs94122@naver.com>
2023-02-09 11:01:11 +0900

    [한상선] 스프링 MVC 1편 - 스프링 MVC 기본기능 - HTTP 요청 파라미터 - @RequestParam까지

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,83 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    /**
+     * 서블릿 시절 사용하던 쿼리 스트링 추출 방식
+     */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * RequestParam 애노테이션을 활용해 내부 속성으로 쿼리 스트링의 Key를 작성해서 해당 key 의 value 추출
+     */
+    @RequestMapping("/request-param-v2")
+    @ResponseBody
+    public String requestParamV2(@RequestParam("username") String memberName, @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * 매개변수의 이름이 쿼리파라미터의 key와 이름이 동일하면 속성을 빼도 동작한다.
+     */
+    @RequestMapping("/request-param-v3")
+    @ResponseBody
+    public String requestParamV3(@RequestParam String username, @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * 쿼리 파라미터의 Key가 일치하면 애노테이션을 제거해도 동작한다.
+     */
+    @RequestMapping("/request-param-v4")
+    @ResponseBody
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-required")
+    @ResponseBody
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-default")
+    @ResponseBody
+    public String requestParamDefault(@RequestParam(defaultValue = "catsbi") String username,
+                                      @RequestParam(defaultValue = "20") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-map")
+    @ResponseBody
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+}
